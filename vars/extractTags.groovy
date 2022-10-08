9ee182dcc10fb2180132cb7aa64f4151a22e99f9
Hemant Tumbare <hemant_tumbare@hotmail.com>
2022-10-08 23:07:41 +0530

    Update extractTags.groovy

--- vars/extractTags.groovy
+++ vars/extractTags.groovy
@@ -1,4 +1,8 @@
 def readFileLineByLine(String str) {
    // String textStr[] = str.split("\\r\\n|\\n|\\r");
-    println str
+   
+   s = str=~/(@[\w-]+)/
+
+while (s.find()) println s.group()
+   
 }
