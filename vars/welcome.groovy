a74628e51580dc1be1c004eccb2aad455cbbb3f9
Hemant Tumbare <hemant_tumbare@hotmail.com>
2022-10-08 22:54:11 +0530

    removed method

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,14 +1,3 @@
 def call (name){
   echo "Hey ${name}, welcome...!!!"
 }
-
-int readFileLineByLine( File file) {
-    def line, noOfLines = 0;
-    file.withReader { reader ->
-        while ((line = reader.readLine()) != null) {
-            println "${line}"
-            noOfLines++
-        }
-    }
-    return noOfLines
-}
