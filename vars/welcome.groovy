c9ce45fead9400b6c9efcc430e4b591d3e1d5179
Hemant Tumbare <hemant_tumbare@hotmail.com>
2022-10-08 19:39:32 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,15 @@
 def call (name){
   echo "Hey ${name}, welcome...!!!"
 }
+
+int readFileLineByLine(String filePath) {
+    File file = new File(filePath)
+    def line, noOfLines = 0;
+    file.withReader { reader ->
+        while ((line = reader.readLine()) != null) {
+            println "${line}"
+            noOfLines++
+        }
+    }
+    return noOfLines
+}
